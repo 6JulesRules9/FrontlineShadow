9ff97ec1af6d6fc69e5a2561d9630b1a208245fa
6JulesRules9 <197079060+6JulesRules9@users.noreply.github.com>
2026-06-26 18:21:24 +0200

    x

--- Assets/_Project/Scripts/Core/IService.cs
+++
@@ -1,9 +0,0 @@
-namespace FrontlineShadow.Core
-{
-    /// <summary>
-    /// Marker-Interface für alle langlebigen Spiel-Services
-    /// (Save, Economy, Repair, TechTree, ...). Werden vom Bootstrap im
-    /// <see cref="ServiceLocator"/> registriert; Systeme/UI lesen nur.
-    /// </summary>
-    public interface IService { }
-}
